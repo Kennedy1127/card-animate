1fb0f1a7c43bc082ba5ed209b5ce4076df1af8bf
Kennedy Chin <zxc1258741@gmail.com>
2023-07-16 12:26:44 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <CardCarousel />
-  <!-- <CardCarousel2 /> -->
 </template>
 
 <script>
 import CardCarousel from "./components/CardCarousel.vue";
-import CardCarousel2 from "./components/CardCarousel2.vue";
 
 export default {
   name: "App",
   components: {
     CardCarousel,
-    CardCarousel2,
   },
 };
 </script>
